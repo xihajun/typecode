df10399c493af55f667681ff54a92f3616faecc0
xihajun <www.junfan.com@gmail.com>
2023-09-26 17:21:52 +0100

    Update typing pool

---
+++ code/vgg.py
@@ -0,0 +1,38 @@
+class VGG(nn.Module):
+  ARCH = [64, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+
+  def __init__(self) -> None:
+    super().__init__()
+
+    layers = []
+    counts = defaultdict(int)
+
+    def add(name: str, layer: nn.Module) -> None:
+      layers.append((f"{name}{counts[name]}", layer))
+      counts[name] += 1
+
+    in_channels = 3
+    for x in self.ARCH:
+      if x != 'M':
+        # conv-bn-relu
+        add("conv", nn.Conv2d(in_channels, x, 3, padding=1, bias=False))
+        add("bn", nn.BatchNorm2d(x))
+        add("relu", nn.ReLU(True))
+        in_channels = x
+      else:
+        # maxpool
+        add("pool", nn.MaxPool2d(2))
+
+    self.backbone = nn.Sequential(OrderedDict(layers))
+    self.classifier = nn.Linear(512, 10)
+
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    # backbone: [N, 3, 32, 32] => [N, 512, 2, 2]
+    x = self.backbone(x)
+
+    # avgpool: [N, 512, 2, 2] => [N, 512]
+    x = x.mean([2, 3])
+
+    # classifier: [N, 512] => [N, 10]
+    x = self.classifier(x)
+    return x
